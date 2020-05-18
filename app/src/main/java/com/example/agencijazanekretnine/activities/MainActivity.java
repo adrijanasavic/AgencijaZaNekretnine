f709973223495479d11c41f377685c7d1423f869
Adrijana <adrijana.jovicic@gmail.com>
2020-05-18 14:18:45 +0200

    toolbar and drawer

--- app/src/main/java/com/example/agencijazanekretnine/activities/MainActivity.java
+++ app/src/main/java/com/example/agencijazanekretnine/activities/MainActivity.java
@@ -1,16 +1,116 @@
 package com.example.agencijazanekretnine.activities;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.example.agencijazanekretnine.R;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ArrayList<String> drawerItems;
+    private ListView drawerList;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private RelativeLayout drawerPane;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        fillDataDrawer();
+        setupToolbar();
+        setupDrawer();
     }
+
+    private void setupDrawer() {
+        drawerList = findViewById( R.id.left_drawer );
+        drawerLayout = findViewById( R.id.drawer_layout );
+        drawerPane = findViewById( R.id.drawerPane );
+        drawerList.setAdapter( new ArrayAdapter<String>( this, android.R.layout.simple_list_item_1, drawerItems ) );
+
+        drawerList.setOnItemClickListener( new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String title = "Unknown";
+                switch (i) {
+                    case 0:
+                        title = "Lista nekretnina";
+                        break;
+                    case 1:
+                        Toast.makeText( getBaseContext(), "Prikaz podesavanja", Toast.LENGTH_SHORT );
+                        title = "Settings";
+                        break;
+                    case 2:
+                        Toast.makeText( getBaseContext(), "Prikaz o aplikaciji", Toast.LENGTH_SHORT );
+                        title = "O aplikaciji";
+                        break;
+                    default:
+                        break;
+                }
+                setTitle( title );
+                drawerLayout.closeDrawer( Gravity.LEFT );
+            }
+        } );
+
+        drawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.app_name,
+                R.string.app_name
+        ) {
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle( "" );
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle( "" );
+                invalidateOptionsMenu();
+            }
+        };
+    }
+
+    private void fillDataDrawer() {
+        drawerItems = new ArrayList<>();
+        drawerItems.add( "Lista nekretnina" );
+        drawerItems.add( "Settings" );
+        drawerItems.add( "O aplikaciji" );
+    }
+
+    public void setupToolbar() {
+        toolbar = findViewById( R.id.toolbar );
+        setSupportActionBar( toolbar );
+        toolbar.setTitleTextColor( Color.WHITE );
+        toolbar.setSubtitle( "Lista nekretnina" );
+        toolbar.setLogo( R.drawable.heart );
+
+        final ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled( true );
+            actionBar.setHomeAsUpIndicator( R.drawable.drawer );
+            actionBar.setHomeButtonEnabled( true );
+            actionBar.show();
+        }
+    }
+
 }
